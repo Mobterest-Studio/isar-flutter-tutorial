eb17c7dba5c5b7133c032a5ce175e22b7ae51901
Mobterest <mobterest@gmail.com>
2022-03-04 13:37:57 +0300

    designing collections

---
+++ lib/collections/category.dart
@@ -0,0 +1,6 @@
+import 'package:isar/isar.dart';
+
+@Collection()
+class Category {
+  late String name;
+}
---
+++ lib/collections/routine.dart
@@ -0,0 +1,9 @@
+import 'package:isar/isar.dart';
+
+@Collection()
+class Routine {
+  late String title;
+  late DateTime startTime;
+  late String day;
+  late String category;
+}
